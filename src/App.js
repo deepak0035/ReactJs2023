a52b7df786ab32a0c027afec8d4a7a21ac11824d
deepak0035 <kingdk0035@gmail.com>
2023-04-25 16:52:42 +0500

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { Fragment, useState } from "react";
+import React from "react";
 import UseState from "./Components/UseState/UseState";
 import UseEffects from "./Components/UseEffects/UseEffects";
 import UseContext from "./Components/UseContext/UseContext";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   const newData = {
     name: "Deepak",
     age: 20,
     address: "123 Main St",
-  }
+  };
   return (
     <>
       <UseContext.Provider value={newData}>
+        <Navbar />
         <UseState />
         <UseEffects />
       </UseContext.Provider>
